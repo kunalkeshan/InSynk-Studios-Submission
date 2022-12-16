0fed9e04db4e32404bab9124b39f20d1de769771
Kunal Keshan <kunalkeshan12@gmail.com>
2022-12-16 21:59:49 +0530

    chore: footer layout added with responsiveness

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,27 +4,28 @@
 
 // Dependencies
 import React from 'react';
+import '../../css/Footer.css';
 
 const Footer = () => {
 	return (
 		<footer>
-			&copy; {new Date().getFullYear()}.{' '}
-			<a
-				href='https://www.linkedin.com/in/kunalkeshan/'
-				target='_blank'
-				rel='noreferrer'
-			>
-				Kunal Keshan
-			</a>
-			.{' '}
-			<a
-				href='https://github.com/kunalkeshan/InSynk-Studios-Submission'
-				target='_blank'
-				rel='noreferrer'
-			>
-				Licensed under MIT
-			</a>
-			.
+			<div className='container'>
+				<p>&copy; {new Date().getFullYear()}.</p>
+				<a
+					href='https://www.linkedin.com/in/kunalkeshan/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Kunal Keshan.
+				</a>
+				<a
+					href='https://github.com/kunalkeshan/InSynk-Studios-Submission'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Licensed under MIT.
+				</a>
+			</div>
 		</footer>
 	);
 };
